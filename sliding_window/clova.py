da8f28ddc06146fec4931e6700416714fbe8e512
JaeHyun KIM <112835087+jh941213@users.noreply.github.com>
2024-07-01 09:57:38 +0900

    Add files via upload

---
+++ sliding_window/clova.py
@@ -0,0 +1,83 @@
+from typing import Any, List, Mapping, Optional
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain_core.language_models.llms import LLM
+import json
+import requests
+
+class LlmClovaStudio(LLM):
+    """
+    Custom LLM class for using the ClovaStudio API.
+    """
+    host: str
+    api_key: str
+    api_key_primary_val: str
+    request_id: str
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.host = kwargs.get('host')
+        self.api_key = kwargs.get('api_key')
+        self.api_key_primary_val = kwargs.get('api_key_primary_val')
+        self.request_id = kwargs.get('request_id')
+
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+    def _call(self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None
+    ) -> str:
+        """
+        Make an API call to the ClovaStudio endpoint using the specified 
+        prompt and return the response.
+        """
+        if stop is not None:
+            raise ValueError("stop kwargs are not permitted.")
+
+        headers = {
+            "X-NCP-CLOVASTUDIO-API-KEY": self.api_key,
+            "X-NCP-APIGW-API-KEY": self.api_key_primary_val,
+            "X-NCP-CLOVASTUDIO-REQUEST-ID": self.request_id,
+            "Content-Type": "application/json; charset=utf-8",
+            "Accept": "text/event-stream"
+        }
+
+        sys_prompt = '''
+        당신은 해적왕 루피입니다. 패기있고 당당하게 답변하세요.
+        user 의 질문이 들어오면 마지막 질문은, 유저의 질문이고 앞에는 이전 대화내역입니다. 문맥에 맞게 잘 답변하세요
+        '''
+
+        preset_text = [{"role": "system", "content": sys_prompt}, {"role": "user", "content": prompt}]
+
+        request_data = {
+            "messages": preset_text,
+            "topP": 0.6,
+            "topK": 0,
+            "maxTokens": 1024,
+            "temperature": 0.5,
+            "repeatPenalty": 1.2,
+            "stopBefore": [],
+            "includeAiFilters": False
+        }
+
+        response = requests.post(
+            self.host + "your url",  # 본인 튜닝 API 경로 사용도 가능 test App 에서
+            headers=headers,
+            json=request_data,
+            stream=True
+        )
+
+        # 스트림에서 마지막 'data:' 라인을 찾기 위한 로직
+        last_data_content = ""
+
+        for line in response.iter_lines():
+            if line:
+                decoded_line = line.decode("utf-8")
+                if '"data":"[DONE]"' in decoded_line:
+                    break
+                if decoded_line.startswith("data:"):
+                    last_data_content = json.loads(decoded_line[5:])["message"]["content"]
+
+        return last_data_content
